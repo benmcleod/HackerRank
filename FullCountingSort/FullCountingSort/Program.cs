079f1e9e19938c790fa653426fa6df0584b030ce
Ben Mcleod <benfmcleod@gmail.com>
2018-12-11 08:39:13 -0500

    Full Counting Sort

---
+++ FullCountingSort/FullCountingSort/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FullCountingSort
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+        public static string countSort(List<List<string>> arr)
+        {
+            StringBuilder[] sortedInput = new StringBuilder[100];
+            for (int i = 0; i < 100; i++)
+            {
+                sortedInput[i] = new StringBuilder();
+            }
+
+            for (int i = 0; i < arr.Count() / 2; i++)
+            {
+                sortedInput[int.Parse(arr[i][0], CultureInfo.InvariantCulture)].Append("- ");
+            }
+            for (int i = arr.Count() / 2; i < arr.Count(); i++)
+            {
+                sortedInput[int.Parse(arr[i][0], CultureInfo.InvariantCulture)].Append(arr[i][1]).Append(" ");
+
+            }
+
+            string result = "";
+
+            foreach (var item in sortedInput)
+            {
+                result += item;
+                
+            }
+
+
+
+            return result.TrimEnd();
+        }
+    }
+}
---
+++ FullCountingSort/UnitTestProject1/UnitTest1.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using Xunit;
+using FullCountingSort;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace UnitTestProject1
+{
+    public class UnitTest1
+    {
+        [Theory]
+        [InlineData(20,@"C:\Users\Ben\source\repos\FullCountingSort\UnitTestProject1\TextFile1.txt", "- - - - - to be or not to be - that is the question - - - -")]
+        public void TestMethod1(int n, string path, string expected)
+        {
+            string[] input = File.ReadAllLines(path);
+
+
+            List<List<string>> arr = new List<List<string>>();
+
+            for (int i = 0; i < n; i++)
+            {
+                arr.Add(input[i].TrimEnd().Split(' ').ToList());
+            }
+
+            var actual = Program.countSort(arr);
+            Assert.Equal(expected, actual);
+
+        }
+    }
+}
